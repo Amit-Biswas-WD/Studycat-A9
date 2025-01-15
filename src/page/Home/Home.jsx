415eb012cce64bd65bb3e38fdb36b2105d58608b
Amit biswas <amitbiswas9282@gmail.com>
2025-01-15 14:46:18 +0600

    Try Studycat for free

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../../components/Banner/Banner";
 import ExploreLanguages from "../../components/ExploreLanguages/ExploreLanguages";
+import StudycatFree from "../../components/StudycatFree/StudycatFree";
 import StudyCatWorks from "../../components/StudycatWorks/StudycatWorks";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
       <Banner />
       <StudyCatWorks />
       <ExploreLanguages/>
+      <StudycatFree/>
     </div>
   );
 };
