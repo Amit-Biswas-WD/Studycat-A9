453b664c8de7b3e353365bc5e9072a8198fff0d2
Amit biswas <amitbiswas9282@gmail.com>
2025-01-17 00:33:27 +0600

    Languages for kids

--- src/components/LanguageKids/LanguageKids.jsx
+++ src/components/LanguageKids/LanguageKids.jsx
@@ -1,3 +1,5 @@
+import FluencyStartsHere from "../FluencyStartsHere/FluencyStartsHere";
+import PawsApplause from "../PawsApplause/PawsApplause";
 import Banner from "./Banner/Banner";
 import ExpertisePlay from "./ExpertisePlay/ExpertisePlay";
 import LessonAdventure from "./LessionAdventure/LessonAdventure";
@@ -10,8 +12,12 @@ const LanguageKids = () => {
         <Banner />
       </section>
       <LessonAdventure />
-      <UnlockingFluency/>
-      <ExpertisePlay/>
+      <UnlockingFluency />
+      <section className="mb-16 mt-32">
+        <FluencyStartsHere />
+      </section>
+      <ExpertisePlay />
+      <PawsApplause />
     </div>
   );
 };
